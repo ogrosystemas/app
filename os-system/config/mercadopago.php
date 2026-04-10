f04c9acf15da847f5a002e8484cd9170f19e515f
ogrosystemas <tibabarcelos@gmail.com>
2026-04-10 18:35:52 -0300

    Update mercadopago.php

--- os-system/config/mercadopago.php
+++ os-system/config/mercadopago.php
@@ -2,5 +2,5 @@
 return array (
   'mp_access_token' => '',
   'mp_device_id' => '',
-  'mp_webhook_secret' => 'c0466cba2cf4c425b8f7c30c486df7ee6c4fe954f7484189890f631d1a8feedd',
+  'mp_webhook_secret' => '',
 );
