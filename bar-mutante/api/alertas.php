91fccc0a3d37cbc17fa2551249b1e4351e7c3927
ogrosystemas <tibabarcelos@gmail.com>
2026-04-10 17:47:06 -0300

    Delete bar-mutante directory

--- bar-mutante/api/alertas.php
+++
@@ -1,9 +0,0 @@
-<?php
-ob_start();
-ini_set("display_errors","0");
-require_once __DIR__ . '/../config/config.php';
-require_once __DIR__ . '/../includes/DB.php';
-require_once __DIR__ . '/../includes/helpers.php';
-header('Content-Type: application/json');
-$alertas = alertasEstoque();
-echo json_encode(['count'=>count($alertas),'items'=>$alertas]);
