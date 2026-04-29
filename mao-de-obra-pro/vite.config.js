b6fa1ebb531b3697e2bea211156b47ecea08c6e1
ogrosystemas <tibabarcelos@gmail.com>
2026-04-29 14:31:35 -0300

    Update vite.config.js

--- mao-de-obra-pro/vite.config.js
+++ mao-de-obra-pro/vite.config.js
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
-import path from 'path'
 
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({ registerType: 'autoUpdate' })
   ],
-  resolve: {
-    alias: {
-      // Isso aqui diz: toda vez que eu usar @, olhe para a pasta src
-      '@': path.resolve(__dirname, './src'),
-    },
+  server: {
+    port: 3000
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  }
 })
