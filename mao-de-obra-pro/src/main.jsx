71c3dad930111efcedb4046b947b39a9c0e9e583
ogrosystemas <tibabarcelos@gmail.com>
2026-04-29 14:47:09 -0300

    Update main.jsx

--- mao-de-obra-pro/src/main.jsx
+++ mao-de-obra-pro/src/main.jsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// Register service worker for PWA
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
