e96571bb4d69f3860917ff686ad7b51643c6411b
ogrosystemas <tibabarcelos@gmail.com>
2026-05-07 17:33:58 -0300

    Update app.js

--- cutelaria-os/js/app.js
+++ cutelaria-os/js/app.js
@@ -30,6 +30,24 @@ import {
   orcamentoPage
 } from './pages/orcamento.js';
 
+import { initRouter } from './modules/router.js';
+
+document.addEventListener(
+  'DOMContentLoaded',
+  async () => {
+
+    await initRouter();
+
+    // LUCIDE ICONS
+
+    if (window.lucide) {
+
+      lucide.createIcons();
+
+    }
+
+  }
+);
 const app =
   document.getElementById('app');
 
