295bde5e44390716db853277b9973c63371754c7
ogrosystemas <tibabarcelos@gmail.com>
2026-05-07 16:21:57 -0300

    Update etapas.service.js

--- cutelaria-os/js/services/etapas.service.js
+++ cutelaria-os/js/services/etapas.service.js
@@ -1,17 +1,36 @@
-export function calcularEtapas(etapas = []) {
+export function calcularEtapas(
+  etapas = [],
+  equipamentos = []
+) {
 
   return etapas.map(etapa => {
+
+    // MÃO DE OBRA
 
     const maoObra =
       etapa.horas * etapa.valorHora;
 
+    // DEPRECIAÇÃO EQUIPAMENTOS
+
+    const custoEquipamentos =
+      equipamentos.reduce((total, equipamento) => {
+
+        return total +
+          (equipamento.custoHora * etapa.horas);
+
+      }, 0);
+
+    // TOTAL
+
     const custoTotal =
       maoObra +
       etapa.custoEnergia +
-      etapa.custoAbrasivos;
+      etapa.custoAbrasivos +
+      custoEquipamentos;
 
     return {
       ...etapa,
+      custoEquipamentos,
       custoTotal
     };
 
