262c7c772a37dd0fa8f306de92158c8f5e0b522f
ogrosystemas <tibabarcelos@gmail.com>
2026-05-07 18:00:11 -0300

    Update app.js

--- cutelaria-os/js/app.js
+++ cutelaria-os/js/app.js
@@ -1,172 +1,18 @@
-import {
-  dashboardPage
-} from './pages/dashboard.js';
+import { initRouter } from './modules/router.js';
 
-import {
-  materiaisPage
-} from './pages/materiais.js';
+document.addEventListener(
+  'DOMContentLoaded',
+  async () => {
 
-import {
-  producaoPage
-} from './pages/producao.js';
+    await initRouter();
 
-import {
-  financeiroPage
-} from './pages/financeiro.js';
+    // LUCIDE ICONS
 
-import {
-  clientesPage
-} from './pages/clientes.js';
+    if (window.lucide) {
 
-import {
-  timelinePage
-} from './pages/timeline.js';
+      lucide.createIcons();
 
-import {
-  configuracoesPage
-} from './pages/configuracoes.js';
-
-import {
-  orcamentoPage
-} from './pages/orcamento.js';
-
-
-const app =
-  document.getElementById('app');
-
-async function renderRoute() {
-
-  const hash =
-    window.location.hash || '#dashboard';
-
-  if (
-    hash.startsWith(
-      '#orcamento/'
-    )
-  ) {
-
-    const id =
-      hash.split('/')[1];
-
-    app.innerHTML =
-      await orcamentoPage(id);
-
-    return;
+    }
 
   }
-
-  const routes = {
-
-    '#dashboard':
-      dashboardPage,
-
-    '#materiais':
-      materiaisPage,
-
-    '#producao':
-      producaoPage,
-
-    '#timeline':
-      timelinePage,
-
-    '#financeiro':
-      financeiroPage,
-
-    '#clientes':
-      clientesPage,
-
-    '#configuracoes':
-      configuracoesPage
-
-  };
-
-  const page =
-    routes[hash];
-
-  if (!page) return;
-
-  app.classList.add(
-    'page-transition'
-  );
-
-  const content =
-    await page();
-
-  setTimeout(() => {
-
-    app.innerHTML = content;
-
-    app.classList.remove(
-      'page-transition'
-    );
-
-  }, 120);
-
-  renderNavbar(hash);
-
-}
-
-function renderNavbar(active) {
-
-  const nav =
-    document.getElementById(
-      'bottomNav'
-    );
-
-  nav.innerHTML = `
-
-    <a
-      href="#dashboard"
-      class="${active === '#dashboard' ? 'active' : ''}"
-    >
-      Dashboard
-    </a>
-
-    <a
-      href="#materiais"
-      class="${active === '#materiais' ? 'active' : ''}"
-    >
-      Materiais
-    </a>
-
-    <a
-      href="#timeline"
-      class="${active === '#timeline' ? 'active' : ''}"
-    >
-      Produção
-    </a>
-
-    <a
-      href="#financeiro"
-      class="${active === '#financeiro' ? 'active' : ''}"
-    >
-      Financeiro
-    </a>
-
-    <a
-      href="#clientes"
-      class="${active === '#clientes' ? 'active' : ''}"
-    >
-      Clientes
-    </a>
-
-    <a
-      href="#configuracoes"
-      class="${active === '#configuracoes' ? 'active' : ''}"
-    >
-      Config
-    </a>
-
-  `;
-
-}
-
-window.addEventListener(
-  'hashchange',
-  renderRoute
-);
-
-window.addEventListener(
-  'load',
-  renderRoute
 );
