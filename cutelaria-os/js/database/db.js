f08646f3edbe02f5a8490e60f9bcbfcdfc0f53db
ogrosystemas <tibabarcelos@gmail.com>
2026-05-07 15:57:17 -0300

    Update db.js

--- cutelaria-os/js/database/db.js
+++ cutelaria-os/js/database/db.js
@@ -1,7 +1,27 @@
 export const db = new Dexie('cutelariaOS');
 
 db.version(1).stores({
-  materiais: '++id,nome,categoria',
-  producoes: '++id,nome,data',
-  equipamentos: '++id,nome',
+
+  materiais: `
+    ++id,
+    nome,
+    categoria,
+    unidade,
+    valor,
+    estoque
+  `,
+
+  equipamentos: `
+    ++id,
+    nome,
+    valorCompra
+  `,
+
+  composicoes: `
+    ++id,
+    nome,
+    categoria,
+    valorFinal
+  `
+
 });
