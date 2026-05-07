1332c27b3bdfe933bd74fb6435ff521c0439764c
ogrosystemas <tibabarcelos@gmail.com>
2026-05-07 16:35:13 -0300

    Update sw.js

--- cutelaria-os/sw.js
+++ cutelaria-os/sw.js
@@ -1,22 +1,89 @@
-const CACHE_NAME = 'cutelaria-os-v1';
+const CACHE_NAME =
+  'cutelaria-os-v1';
 
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/css/main.css',
-  '/js/app.js'
+
+  './',
+
+  './index.html',
+
+  './manifest.json',
+
+  './css/main.css',
+  './css/layout.css',
+  './css/components.css',
+  './css/pages.css',
+  './css/animations.css',
+
+  './js/app.js',
+
+  './assets/icons/icon-192.png',
+  './assets/icons/icon-512.png'
 ];
 
-self.addEventListener('install', event => {
+// INSTALL
+
+self.addEventListener('install', (event) => {
+
   event.waitUntil(
+
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then((cache) => {
+
+        return cache.addAll(
+          urlsToCache
+        );
+
+      })
+
   );
+
 });
 
-self.addEventListener('fetch', event => {
+// FETCH
+
+self.addEventListener('fetch', (event) => {
+
   event.respondWith(
+
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+
+      .then((response) => {
+
+        return response || fetch(event.request);
+
+      })
+
   );
+
+});
+
+// ACTIVATE
+
+self.addEventListener('activate', (event) => {
+
+  event.waitUntil(
+
+    caches.keys()
+
+      .then((cacheNames) => {
+
+        return Promise.all(
+
+          cacheNames.map((cache) => {
+
+            if (cache !== CACHE_NAME) {
+
+              return caches.delete(cache);
+
+            }
+
+          })
+
+        );
+
+      })
+
+  );
+
 });
