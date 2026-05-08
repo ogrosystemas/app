6ef0a56fe9199f3767c564fba88156b201271a34
ogrosystemas <tibabarcelos@gmail.com>
2026-05-08 16:51:35 -0300

    Update db.js

--- cutelaria-os/js/database/db.js
+++ cutelaria-os/js/database/db.js
@@ -2,6 +2,41 @@ export const db = new Dexie(
   'cutelariaOS'
 );
 
+// ======================
+// VERSION 1
+// ======================
+
+db.version(1).stores({
+
+  materiais:
+    '++id,nome,categoria,valor'
+
+});
+
+// ======================
+// VERSION 2
+// ======================
+
+db.version(2).stores({
+
+  materiais:
+    '++id,nome,categoria,valor',
+
+  composicoes:
+    '++id,nome,tipoAco,valorFinal',
+
+  financeiro:
+    '++id,tipo,valor,data',
+
+  clientes:
+    '++id,nome,telefone'
+
+});
+
+// ======================
+// VERSION 3
+// ======================
+
 db.version(3).stores({
 
   materiais:
@@ -21,4 +56,16 @@ db.version(3).stores({
 
 });
 
-db.open();
+// ======================
+// OPEN DATABASE
+// ======================
+
+db.open()
+  .catch((error) => {
+
+    console.error(
+      'Erro IndexedDB:',
+      error
+    );
+
+  });
