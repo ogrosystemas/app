9d8ca3b20d482c0c891d4e237eab87946bb0f968
ogrosystemas <tibabarcelos@gmail.com>
2026-05-08 16:05:53 -0300

    new

--- cutelaria-os/js/core/router.js
+++ cutelaria-os/js/core/router.js
@@ -1,31 +1,99 @@
-const routes = {};
+const routes = {
 
-export function registerRoute(name, render) {
-  routes[name] = render;
+  dashboard: './pages/dashboard.js',
+  materiais: './pages/materiais.js',
+  producao: './pages/producao.js',
+  financeiro: './pages/financeiro.js',
+  clientes: './pages/clientes.js',
+  config: './pages/configuracoes.js'
+
+};
+
+async function renderRoute() {
+
+  const app =
+    document.getElementById('app');
+
+  let hash =
+    window.location.hash
+      .replace('#', '');
+
+  if (!hash) {
+
+    hash = 'dashboard';
+
+  }
+
+  if (!routes[hash]) {
+
+    hash = 'dashboard';
+
+  }
+
+  try {
+
+    const module =
+      await import(routes[hash]);
+
+    const renderFunction =
+      Object.values(module)[0];
+
+    const html =
+      await renderFunction();
+
+    app.innerHTML = `
+
+      <main class="page-transition">
+
+        ${html}
+
+      </main>
+
+    `;
+
+    // RECRIAR LUCIDE
+
+    if (window.lucide) {
+
+      lucide.createIcons();
+
+    }
+
+  } catch (error) {
+
+    console.error(error);
+
+    app.innerHTML = `
+
+      <div class="card">
+
+        <h1 style="
+          font-size:28px;
+          margin-bottom:20px;
+        ">
+          Erro no Router
+        </h1>
+
+        <pre style="
+          color:#f97316;
+          white-space:pre-wrap;
+        ">${error}</pre>
+
+      </div>
+
+    `;
+
+  }
+
 }
 
-export async function loadRoute() {
+export async function initRouter() {
 
-  const route =
-    window.location.hash.replace('#', '') || 'dashboard';
+  window.addEventListener(
+    'hashchange',
+    renderRoute
+  );
 
-  const view = document.getElementById('view');
+  await renderRoute();
 
-  if (!view) return;
-
-  if (routes[route]) {
-
-    const content = await routes[route]();
-
-    view.innerHTML = content;
-
-  } else {
-
-    view.innerHTML = `
-      <div class="card">
-        <h2>404</h2>
-        <p>Página não encontrada.</p>
-      </div>
-    `;
-  }
 }
