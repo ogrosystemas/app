b3180e3b9bae47c1520d6b29a89c18cfa768e9ba
ogrosystemas <tibabarcelos@gmail.com>
2026-05-08 16:39:15 -0300

    Update db.js

--- cutelaria-os/js/database/db.js
+++ cutelaria-os/js/database/db.js
@@ -1,149 +1,22 @@
 export const db = new Dexie(
-  'CutelariaOS'
+  'cutelariaOS'
 );
 
-db.version(6).stores({
-
-  clientes:
-    `
-      ++id,
-      nome,
-      telefone,
-      email,
-      instagram,
-      cidade,
-      observacoes,
-      createdAt
-    `,
-
-  pedidos:
-    `
-      ++id,
-      clienteId,
-      composicaoId,
-
-      titulo,
-
-      status,
-
-      valor,
-      entrada,
-      restante,
-
-      prioridade,
-
-      entregaPrevista,
-
-      progresso,
-
-      observacoes,
-
-      createdAt
-    `,
-
-  timeline:
-    `
-      ++id,
-      pedidoId,
-      etapa,
-      descricao,
-      foto,
-      concluido,
-      createdAt
-    `,
+db.version(1).stores({
 
   materiais:
-    `
-      ++id,
-      nome,
-      categoria,
-      valor,
-      unidade,
-      estoqueAtual,
-      estoqueMinimo,
-      createdAt
-    `,
-
-  equipamentos:
-    `
-      ++id,
-      nome,
-      valorCompra,
-      vidaUtil,
-      horasMes,
-      custoHora
-    `,
+    '++id,nome,categoria,valor',
 
   composicoes:
-    `
-      ++id,
-      nome,
-      categoria,
-
-      tipoFaca,
-      tipoAco,
-      hrc,
-      espessura,
-      comprimento,
-      peso,
-      acabamento,
-      desbaste,
-      tipoCabo,
-      possuiBainha,
-      observacoes,
-
-      fotoCapa,
-
-      custoMateriais,
-      custoEtapas,
-      custoTotal,
-      margemLucro,
-      valorFinal,
-      createdAt
-    `,
-
-  composicaoItens:
-    `
-      ++id,
-      composicaoId,
-      materialId,
-      nome,
-      quantidade,
-      valorUnitario,
-      subtotal
-    `,
-
-  etapas:
-    `
-      ++id,
-      composicaoId,
-      nome,
-      horas,
-      valorHora,
-      custoEquipamentos,
-      custoEnergia,
-      custoAbrasivos,
-      custoTotal
-    `,
-
-  fotos:
-    `
-      ++id,
-      composicaoId,
-      imagem,
-      createdAt
-    `,
+    '++id,nome,tipoAco,valorFinal',
 
   financeiro:
-    `
-      ++id,
-      tipo,
-      categoria,
-      descricao,
-      valor,
-      vencimento,
-      status,
-      createdAt
-    `
+    '++id,tipo,valor,data',
+
+  clientes:
+    '++id,nome,telefone',
+
+  settings:
+    '++id,oficinaNome'
 
 });
