846dffc930d718bee178c2f5fabe2415ae4628ae
ogrosystemas <tibabarcelos@gmail.com>
2026-06-23 08:55:26 -0300

    del

--- mensalidades-src/src/hooks/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./useConfig";
-export * from "./useMembros";
-export * from "./usePagamentos";
-export * from "./useInadimplencia";
-export * from "./useDashboardResumo";
-export * from "./useBackup";
-export * from "./useRelatorio";
